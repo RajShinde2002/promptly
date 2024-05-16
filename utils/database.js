d99a1f1886c8d08892b9d75aa23d1426232e8181
RajShinde2002 <rajshinde2128@gmail.com>
2024-05-16 18:25:36 +0530

    Stringify URI

--- utils/database.js
+++ utils/database.js
@@ -11,7 +11,7 @@ export const connectToDB = async () => {
   }
 
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
+    await mongoose.connect("mongodb+srv://rjshinde45:Raj2002@cluster0.thxmbzn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
       dbName: "share_prompt",
     });
     
